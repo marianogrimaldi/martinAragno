c84735f630c3754de83dafce88181aae11647d78
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-06-26 22:37:31 -0300

    aplicacion de mq 3

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -9,8 +9,8 @@ const Header = () => {
             <ul className="navFlex">
                 <NavLink to="/" ><li >HOME</li></NavLink>
                 <NavLink to="/triatlon" ><li>TRIATHLON</li></NavLink> 
-                <NavLink to="/taur" ><li>TRIAL & RUNNING</li></NavLink>
-                <NavLink to="/mtb" ><li>MTB</li></NavLink>
+                 <NavLink to="/mtb" ><li>MOUNTAIN BIKE</li></NavLink>
+               <NavLink to="/taur" ><li>TRIAL & RUNNING</li></NavLink>
                 <NavLink to="/mountain" ><li>MOUNTAIN SUMMIT</li></NavLink>
                 <NavLink to="/calendario" ><li>CALENDARIO 2023</li></NavLink>
                 <NavLink to="/sponsors" ><li>SPONSORS</li></NavLink>
