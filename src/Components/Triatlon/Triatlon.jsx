28240f8fd1c5bba96a49bdabc9b76af116d76cd6
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2023-10-08 18:52:54 -0300

    cambios nuevos con mq landscapemobile

--- src/Components/Triatlon/Triatlon.jsx
+++ src/Components/Triatlon/Triatlon.jsx
@@ -23,7 +23,7 @@ const Triatlon = () => {
                         <h2>MARTÍN ARAGNO</h2>
                         <p >multisport racing</p>
                     </div>
-                    <div className="flexLogoCard">
+                    <div className="flexLogoCardtri">
                         <img src={logo} className="logo" alt="" />
                     </div>
                 
