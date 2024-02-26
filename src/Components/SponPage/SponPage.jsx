d98f3aad686f7f87aa3b464ae3541c158d27f40f
marianogrimaldi <marianogdanielgrimaldi@gmail.com>
2024-02-26 17:21:20 -0300

    cambios en link de pro + agregamos resultado en trail

--- src/Components/SponPage/SponPage.jsx
+++ src/Components/SponPage/SponPage.jsx
@@ -32,32 +32,32 @@ const SponPage = () => {
                 <div className="imgNat">
                     <div className="flexSponPage">
 
-                    <a href="https://2cumbres.com.ar/search/?q=rockshox"target="_blank"><img src={sp12} alt="" /></a>
-                    <a href="https://ezkteam.com/"target="_blank"><img src={sp3} alt="" /></a>
-                    <a href="https://2cumbres.com.ar/search/?q=zipp"target="_blank"><img src={sp11} alt="" /></a>
-                    <img src={sp10} alt="" />
-                    <img src={sp17} alt="" />
+                        <a href="https://2cumbres.com.ar/search/?q=rockshox" target="_blank"><img src={sp12} alt="" /></a>
+                        <a href="https://ezkteam.com/" target="_blank"><img src={sp3} alt="" /></a>
+                        <a href="https://2cumbres.com.ar/search/?q=zipp" target="_blank"><img src={sp11} alt="" /></a>
+                        <img src={sp10} alt="" />
+                        <a href="https://www.progravityhealth.com/"target="_blank"><img src={sp17} alt="" /></a>
                     </div>
                     <img src={imgNat} className="imgMartin" alt="" />
                     <div className="flexSponPage">
-                    <a href="https://2cumbres.com.ar/" target="_blank"><img src={sp7} alt="" /></a>
-                    <a href="https://premiumtri.com/"target="_blank"><img src={sp15} alt="" /></a>
-                    <a href="https://www.clubsanjorge.com.ar/"target="_blank"><img src={sp9} alt="" /></a>
-                    <a href="https://2cumbres.com.ar/search/?q=sram"target="_blank"><img src={sp6} alt="" /></a>
+                        <a href="https://2cumbres.com.ar/" target="_blank"><img src={sp7} alt="" /></a>
+                        <a href="https://premiumtri.com/" target="_blank"><img src={sp15} alt="" /></a>
+                        <a href="https://www.clubsanjorge.com.ar/" target="_blank"><img src={sp9} alt="" /></a>
+                        <a href="https://2cumbres.com.ar/search/?q=sram" target="_blank"><img src={sp6} alt="" /></a>
 
                     </div>
                 </div>
                 <div className="sponSecGrid">
 
-                <a href="https://www.nutritionred.com.ar/"target="_blank"><img src={sp1} alt="" /></a>
-                <a href="https://www.instagram.com/modofitentrenamientos/"target="_blank"><img src={sp16} className="fit" alt="" /></a>
-                <a href="https://linktr.ee/centroymoa?fbclid=PAAaZJXYoAJKsTioI6-5V4qusHp_MndSWBdwQtDmvMQf69NeOuMPj93dppTN4"target="_blank"><img src={sp8} alt="" /></a>
-                <a href="https://www.instagram.com/cabritoscycling/"target="_blank"><img src={sp20} alt="" /></a>
-                    <a href="https://qualitynutriciondeportiva.com/?fbclid=PAAabOYNGcFBFM9PWB53R4Ptk-2lMMMq_NYIvYR11l0TXCuME3j_e9gCFDbyA"target="_blank"><img src={sp13} alt="" /></a>
+                    <a href="https://www.nutritionred.com.ar/" target="_blank"><img src={sp1} alt="" /></a>
+                    <a href="https://www.instagram.com/modofitentrenamientos/" target="_blank"><img src={sp16} className="fit" alt="" /></a>
+                    <a href="https://linktr.ee/centroymoa?fbclid=PAAaZJXYoAJKsTioI6-5V4qusHp_MndSWBdwQtDmvMQf69NeOuMPj93dppTN4" target="_blank"><img src={sp8} alt="" /></a>
+                    <a href="https://www.instagram.com/cabritoscycling/" target="_blank"><img src={sp20} alt="" /></a>
+                    <a href="https://qualitynutriciondeportiva.com/?fbclid=PAAabOYNGcFBFM9PWB53R4Ptk-2lMMMq_NYIvYR11l0TXCuME3j_e9gCFDbyA" target="_blank"><img src={sp13} alt="" /></a>
                     <div></div>
-                    <a href="https://linktr.ee/Indusparquetarg?fbclid=PAAaa6pG3VjsbOjN8YKZT1e6D2HK7Kotcu_htA4o4xKjysQ7nDZ2IOarKAjuU"target="_blank"><img src={sp19} alt="" /></a>
-                    <a href="https://www.atom.bio/runningtrip/?fbclid=PAAaZhuYIi0Af-MkKfbI8lrXmWLVr1_t8nLKv6Kp0gtpZeVt-1SBwKTNzyzW8"target="_blank"><img src={sp18} alt="" /></a>
-                    <a href="https://footpoint.com.ar/?fbclid=PAAaYO_UK1Qx1BlCnluZkdmYTtNqb9b1A58BRs87Jp7zL-NGun3Mjqfm5kMy0"target="_blank"><img src={sp14} alt="" /></a>
+                    <a href="https://linktr.ee/Indusparquetarg?fbclid=PAAaa6pG3VjsbOjN8YKZT1e6D2HK7Kotcu_htA4o4xKjysQ7nDZ2IOarKAjuU" target="_blank"><img src={sp19} alt="" /></a>
+                    <a href="https://www.atom.bio/runningtrip/?fbclid=PAAaZhuYIi0Af-MkKfbI8lrXmWLVr1_t8nLKv6Kp0gtpZeVt-1SBwKTNzyzW8" target="_blank"><img src={sp18} alt="" /></a>
+                    <a href="https://footpoint.com.ar/?fbclid=PAAaYO_UK1Qx1BlCnluZkdmYTtNqb9b1A58BRs87Jp7zL-NGun3Mjqfm5kMy0" target="_blank"><img src={sp14} alt="" /></a>
                 </div>
             </div>
         </div>
